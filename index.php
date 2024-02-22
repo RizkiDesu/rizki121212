2a8e9fe5e68fa0e879a2b3b0127c202632c740ac
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-02-22 11:14:55 +0700

    tambahkan isi

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hallo Word</h1>
+    <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum odit architecto perspiciatis recusandae quo aliquid rem sapiente quisquam molestias autem corporis quas commodi veritatis, explicabo eveniet a, cum, tempora accusantium?</h1>
 </body>
 </html>
